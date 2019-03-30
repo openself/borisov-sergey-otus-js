d7dc00e24aa8f7087f5bf2d6e3b6459103e0725f
openself <openself@bk.ru>
2019-03-30 14:55:54 +0300

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* global require __dirname module */
+const path = require('path')
+
+const conf = {
+    entry: './dev/lesson01/lesson01.js',
+    
+    output: {
+        path: path.resolve(__dirname, './js'),
+        filename: 'main.js',
+        publicPath: 'js/'
+    },
+    devServer: {
+        overlay: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                // exclude: '/node_modules/'
+            }
+        ]
+    }
+}
+
+module.exports = (env, options) => {
+    conf.devtool = options.mode == "production" ?
+        false :
+        "cheap-module-eval-source-map"
+
+    return conf
+}
