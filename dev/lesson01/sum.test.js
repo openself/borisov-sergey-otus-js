89dc12186997e3baed88cca5ea8c76ac86fc6d39
openself <openself@bk.ru>
2019-03-30 19:23:56 +0300

    lesson 1

---
+++ dev/lesson01/sum.test.js
@@ -0,0 +1,13 @@
+import sum from './lesson01'
+
+test('sum() = 0', () => {
+    expect(sum()).toBe(0);
+})
+
+test('sum(1)() = 1', () => {
+    expect(sum(1)()).toBe(1);
+})
+
+test('sum(10)(20)(30)() = 60', () => {
+    expect(sum(1)()).toBe(1);
+})
