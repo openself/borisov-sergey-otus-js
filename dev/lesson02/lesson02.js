65660dff9268fe350cd96aeff094fdba6cf3c13d
openself <openself@bk.ru>
2019-04-06 16:42:58 +0300

    lesson 2

---
+++ dev/lesson02/lesson02.js
@@ -0,0 +1,12 @@
+const promiseReduce = async (asyncFunctions, reduce, initialValue) => {
+    let accum = initialValue
+    for (let func of asyncFunctions) {
+        let funcResult = await func()
+        accum = await reduce(accum, funcResult)
+        }
+
+    return accum
+}
+
+export default promiseReduce
+
