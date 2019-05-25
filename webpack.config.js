d3c1dc7b84aa9c2b8df96a6da0ffc4166a75809c
openself <openself@bk.ru>
2019-05-25 21:51:18 +0300

    Lesson 10 GraphQL init app

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
 /* global require __dirname module */
-const path = require('path')
-
-const conf = {
-    entry: './dev/lesson07/index.js',
-    
-    output: {
-        path: path.resolve(__dirname, './js'),
-        filename: 'main.js',
-        publicPath: 'js/'
-    },
-    devServer: {
-        overlay: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                // exclude: '/node_modules/'
-            }
-        ]
-    }
-}
-
-module.exports = (env, options) => {
-    conf.devtool = options.mode == "production" ?
-        false :
-        "cheap-module-eval-source-map"
-
-    return conf
-}
+// const path = require('path')
+//
+// const conf = {
+//     entry: './dev/lesson07/index.js',
+//
+//     output: {
+//         path: path.resolve(__dirname, './js'),
+//         filename: 'main.js',
+//         publicPath: 'js/'
+//     },
+//     devServer: {
+//         overlay: true
+//     },
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.js$/,
+//                 loader: 'babel-loader',
+//                 // exclude: '/node_modules/'
+//             }
+//         ]
+//     }
+// }
+//
+// module.exports = (env, options) => {
+//     conf.devtool = options.mode == "production" ?
+//         false :
+//         "cheap-module-eval-source-map"
+//
+//     return conf
+// }
